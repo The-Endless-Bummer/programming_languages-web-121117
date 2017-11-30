6f588eb0487e1edea7d7512900b2e753da9c84d3
Maurice Roy <mautayro@gmail.com>
2017-11-30 19:42:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,14 +1,14 @@
-#require "pry"
+require "pry"
 
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, style_data|
     style_data.each do |language, language_data|
       new_hash[language] = language_data
-      #binding.pry
       if new_hash[language][:style] == nil
         new_hash[language][:style] = []
       end
+      binding.pry
       new_hash[language][:style].push style
     end
   end
