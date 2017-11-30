6f26561f386270e53a2d65c7a9fa148eb571e626
Maurice Roy <mautayro@gmail.com>
2017-11-30 18:54:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   languages.each do |style, style_data|
     style_data.each do |language, language_data|
       new_hash[language] = language_data
-      new_hash[language][:style] = [style]
+      new_hash[language][:style] += [style]
     end
   end
   new_hash
